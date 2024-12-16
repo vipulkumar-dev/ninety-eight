257b3af0dbd459b3f6e2f47b32ceee5fbb2283cd
Vipul Kumar <email.coex@gmail.com>
2024-12-16 06:52:11 +0530

    abc

--- yeildstone/index.js
+++ yeildstone/index.js
@@ -215,7 +215,7 @@ const phaseTl = gsap.timeline({
     start: "center center",
     end: "+=1000",
     markers: true,
-    pin: true,
+    pin: ".section_pin",
     scrub: true,
     onUpdate: (self) => {
       if (self.progress < 0.25) {
