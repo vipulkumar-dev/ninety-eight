412d6be7b43e8197be338d8a19df47ccd2c0f304
Vipul Kumar <email.coex@gmail.com>
2024-12-17 16:29:30 +0530

    abc

--- yeildstone/index.js
+++ yeildstone/index.js
@@ -1,3 +1,13 @@
+import PhotoSwipeLightbox from "https://unpkg.com/photoswipe/dist/photoswipe-lightbox.esm.js";
+
+const lightbox = new PhotoSwipeLightbox({
+  gallery: "#my-gallery",
+  children: "a",
+  pswpModule: () => import("https://unpkg.com/photoswipe"),
+});
+
+lightbox.init();
+
 const header = document.getElementById("header");
 let isMenuOpen = false;
 let lastScrollPosition = 0;
