f54dc51633c75a08b0a7d1989d4c40053ee3cc2a
Vipul Kumar <email.coex@gmail.com>
2025-03-06 23:35:33 +0530

    abc

--- deepnode/index.js
+++ deepnode/index.js
@@ -49,6 +49,80 @@ window.addEventListener("scroll", () => {
 
 console.log("deepnode");
 
+/// Menu
+
+const menu_tl = gsap.timeline({
+  paused: true,
+  defaults: {
+    duration: 0.3,
+    ease: "power3.inOut",
+  },
+});
+
+menu_tl
+  .to(".menu_line", {
+    width: "100%",
+    duration: 0.3,
+  })
+  .to(".menu_line.top", {
+    y: 5,
+    duration: 0.2,
+  })
+  .to(
+    ".menu_line.bottom",
+    {
+      y: -5,
+      duration: 0.2,
+    },
+    "<"
+  )
+  .set(".menu_line.middle", { opacity: 0 })
+  .to(".menu_line.top", {
+    rotate: 45,
+  })
+  .to(
+    ".menu_line.bottom",
+    {
+      rotate: -45,
+    },
+    "<"
+  )
+  .fromTo(
+    ".navigation_wrapper",
+    {
+      y: "-100%",
+    },
+    {
+      y: "0%",
+      duration: 1.2,
+    },
+    "-=0.7"
+  )
+  .fromTo(
+    ".nav_animate",
+    {
+      x: "150px",
+    },
+    {
+      x: "0px",
+      duration: 0.9,
+      stagger: 0.03,
+    },
+    "-=1.1"
+  );
+
+let isMenuOpen = false;
+document.querySelector(".menu_trigger").addEventListener("click", () => {
+  if (!isMenuOpen) {
+    console.log("open");
+    menu_tl.play();
+  } else {
+    console.log("close");
+    menu_tl.reverse();
+  }
+  isMenuOpen = !isMenuOpen;
+});
+
 // Nav
 
 const nav_items = document.querySelectorAll(".nav_item");
