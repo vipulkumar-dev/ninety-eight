035a42fe96673b6b72fc5b458745ab2901af3887
Vipul Kumar <email.coex@gmail.com>
2025-03-21 01:08:14 +0530

    abc

--- biao/index.js
+++ biao/index.js
@@ -1,10 +1,12 @@
-import { roll } from "../utils.js";
+import { roll, getDevices, lenisInit } from "../utils.js";
 import { liveReload } from "../liveReload.js";
+gsap.registerPlugin(ScrollTrigger);
 
 const header = document.getElementById("header");
 let lastScrollPosition = 0;
 let delta = 50; // Minimum scroll distance before toggling header
 let ticking = false;
+const { isDesktop, isMobile } = getDevices();
 
 function handleScroll() {
   const currentScrollPosition = window.scrollY;
@@ -22,6 +24,22 @@ function handleScroll() {
 
   ticking = false;
 }
+
+if (isDesktop) {
+  lenisInit();
+}
+
+const about_dash = gsap.to(".about_dash", {
+  top: "83.3%",
+  ease: "linear",
+  scrollTrigger: {
+    trigger: ".about_content",
+    start: "top 30%",
+    end: "bottom 50%",
+    scrub: 0.3,
+    // markers: true,
+  },
+});
 
 // var scriptLocation = document.currentScript.src;
 // console.log("scriptLocation", scriptLocation);
