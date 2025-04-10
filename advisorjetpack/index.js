b667c8040f8117a7621e41b47170fa7b0910246a
Vipul Kumar <email.coex@gmail.com>
2025-04-10 22:29:40 +0530

    abc

--- advisorjetpack/index.js
+++ advisorjetpack/index.js
@@ -1,10 +1,26 @@
-import {
-  roll,
-  getDevices,
-  lenisInit,
-  convertVhToFixedHeight,
-} from "../utils.js";
+import { roll, getDevices, convertVhToFixedHeight } from "../utils.js";
 import { liveReload } from "../liveReload.js";
+
+function lenisInit() {
+  // Initialize a new Lenis instance for smooth scrolling
+  const lenis = new Lenis({
+    lerp: 0.15,
+  });
+
+  // Synchronize Lenis scrolling with GSAP's ScrollTrigger plugin
+  lenis.on("scroll", ScrollTrigger.update);
+
+  // Add Lenis's requestAnimationFrame (raf) method to GSAP's ticker
+  // This ensures Lenis's smooth scroll animation updates on each GSAP tick
+  gsap.ticker.add((time) => {
+    lenis.raf(time * 1000); // Convert time from seconds to milliseconds
+  });
+
+  // Disable lag smoothing in GSAP to prevent any delay in scroll animations
+  gsap.ticker.lagSmoothing(0);
+
+  return lenis;
+}
 
 const lenis = lenisInit();
 const { isDesktop, isMobile } = getDevices();
