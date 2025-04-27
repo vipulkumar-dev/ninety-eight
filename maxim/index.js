21ac1fcc5a9a1e26c5b47446fc94d972908d528c
Vipul Kumar <email.coex@gmail.com>
2025-04-28 03:16:46 +0530

    abc

---
+++ maxim/index.js
@@ -0,0 +1,64 @@
+import { roll } from "../utils.js";
+import { liveReload } from "../liveReload.js";
+
+const header = document.getElementById("header");
+let lastScrollPosition = 0;
+let delta = 50; // Minimum scroll distance before toggling header
+let ticking = false;
+
+function handleScroll() {
+  const currentScrollPosition = window.scrollY;
+
+  if (Math.abs(currentScrollPosition - lastScrollPosition) > delta) {
+    if (currentScrollPosition > lastScrollPosition) {
+      // Scrolling down
+      header.classList.add("hidden");
+    } else {
+      // Scrolling up
+      header.classList.remove("hidden");
+    }
+    lastScrollPosition = currentScrollPosition;
+  }
+
+  ticking = false;
+}
+
+// var scriptLocation = document.currentScript.src;
+// console.log("scriptLocation", scriptLocation);
+
+window.addEventListener("scroll", () => {
+  if (!ticking) {
+    window.requestAnimationFrame(handleScroll);
+    ticking = true;
+  }
+});
+
+document.querySelectorAll(".swiper").forEach((swiper) => {
+  const swiperInstance = new Swiper(swiper, {
+    // Optional parameters
+    direction: "horizontal",
+    slidesPerView: "auto",
+    spaceBetween: 28,
+    // loop: true,
+
+    // If we need pagination
+    // pagination: {
+    //   el: ".swiper-pagination",
+    // },
+
+    // // Navigation arrows
+    // navigation: {
+    //   nextEl: ".swiper-button-next",
+    //   prevEl: ".swiper-button-prev",
+    // },
+
+    // // And if we need scrollbar
+    // scrollbar: {
+    //   el: ".swiper-scrollbar",
+    // },
+  });
+});
+
+// console.log("From how it why");
+roll("[roll]", 80);
+liveReload();
