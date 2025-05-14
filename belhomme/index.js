1fb5877babb28b4521d32420ecaa5302395e75e1
Vipul Kumar <email.coex@gmail.com>
2025-05-14 07:03:17 +0530

    abc

--- belhomme/index.js
+++ belhomme/index.js
@@ -139,7 +139,7 @@ const parallaxTl = gsap.timeline({
     end: `+=${paralax_amount * 1.2}`,
     scrub: 1,
     pin: true,
-    anticipatePin: 1,
+    anticipatePin: 2,
     // markers: true,
   },
 });
