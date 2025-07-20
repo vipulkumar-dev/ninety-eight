edcf07a108debfc92f903b5daf84630f3e99ba1e
Vipul Kumar <email.coex@gmail.com>
2025-07-21 02:02:23 +0530

    abc

--- qace/index.js
+++ qace/index.js
@@ -383,59 +383,78 @@ gsap.set("[para-reveal]", {
   opacity: 1,
 });
 
-ScrollTrigger.batch("[basic-reveal],[fade-reveal],[para-reveal]", {
-  start: (scrollInstance) => {
-    const eltrigger = scrollInstance.trigger;
-    const eltriggerHeight = eltrigger.clientHeight * 1.4;
-    // if (eltrigger.hasAttribute("basic-reveal")) {
-    //   return `top-=${eltriggerHeight}px bottom`;
-    // }
-    return "top bottom";
-  },
-  end: (scrollInstance) => {
-    const eltrigger = scrollInstance.trigger;
-    const eltriggerHeight = eltrigger.clientHeight * 1.4;
-    // if (eltrigger.hasAttribute("basic-reveal")) {
-    //   return `top-=${eltriggerHeight}px bottom`;
-    // }
-    return "top bottom";
-  },
-  // markers: true,
-  onEnter: (elements, triggers) => {
-    const animateItems = [];
-    let duration = 1;
+const isLoader = true;
 
-    elements.forEach((element) => {
-      if (element.hasAttribute("basic-reveal")) {
-        animateItems.push(element);
-      }
-      if (element.hasAttribute("para-reveal")) {
-        console.log("para", element);
-        element.querySelectorAll(".para_line").forEach((line) => {
-          animateItems.push(line);
-        });
-      }
-      if (element.hasAttribute("fade-reveal")) {
-        animateItems.push(element);
-      }
-    });
-    // console.log("animateItems", animateItems);
+function initReveal() {
+  ScrollTrigger.batch("[basic-reveal],[fade-reveal],[para-reveal]", {
+    start: (scrollInstance) => {
+      const eltrigger = scrollInstance.trigger;
+      const eltriggerHeight = eltrigger.clientHeight * 1.4;
+      // if (eltrigger.hasAttribute("basic-reveal")) {
+      //   return `top-=${eltriggerHeight}px bottom`;
+      // }
+      return "top bottom";
+    },
+    end: (scrollInstance) => {
+      const eltrigger = scrollInstance.trigger;
+      const eltriggerHeight = eltrigger.clientHeight * 1.4;
+      // if (eltrigger.hasAttribute("basic-reveal")) {
+      //   return `top-=${eltriggerHeight}px bottom`;
+      // }
+      return "top bottom";
+    },
+    // markers: true,
+    onEnter: (elements, triggers) => {
+      const animateItems = [];
+      let duration = 1;
 
-    gsap.to(animateItems, {
-      y: "0%",
-      opacity: 1,
-      filter: "blur(0px)",
-      scaleY: 1,
-      stagger: 0.04,
-      duration: (index, target) => {
-        if (target.hasAttribute("extra-time")) {
-          return 2.5;
+      elements.forEach((element) => {
+        if (element.hasAttribute("basic-reveal")) {
+          animateItems.push(element);
         }
-        return 1.5;
-      },
-      ease: "power4.inOut",
-    });
-  },
+        if (element.hasAttribute("para-reveal")) {
+          console.log("para", element);
+          element.querySelectorAll(".para_line").forEach((line) => {
+            animateItems.push(line);
+          });
+        }
+        if (element.hasAttribute("fade-reveal")) {
+          animateItems.push(element);
+        }
+      });
+      // console.log("animateItems", animateItems);
+
+      gsap.to(animateItems, {
+        y: "0%",
+        opacity: 1,
+        filter: "blur(0px)",
+        scaleY: 1,
+        stagger: 0.04,
+        duration: (index, target) => {
+          if (target.hasAttribute("extra-time")) {
+            return 2.5;
+          }
+          return 1.5;
+        },
+        ease: "power4.inOut",
+      });
+    },
+  });
+}
+
+if (isLoader) {
+  setTimeout(() => {
+    initReveal();
+  }, 6400);
+} else {
+  initReveal();
+}
+
+gsap.to("[loading-animation]", {
+  opacity: 0,
+  duration: 0.4,
+  delay: 6.2,
+  ease: "power2.inOut",
 });
 
 liveReload();
