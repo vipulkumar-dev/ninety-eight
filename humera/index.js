314929e7d7687881f523026fc0e140acc78af02d
Vipul Kumar <email.coex@gmail.com>
2025-07-28 15:09:42 +0530

    abc

--- humera/index.js
+++ humera/index.js
@@ -3,6 +3,108 @@ import { liveReload } from "../liveReload.js";
 
 const { isDesktop, isMobile } = getDevices();
 
+const header = document.getElementById("header");
+
+if (header) {
+  let isMenuOpen = false;
+  let lastScrollPosition = 0;
+  let delta = isDesktop ? 30 : 60; // Minimum scroll distance before toggling header
+  let ticking = false;
+
+  function handleScroll() {
+    const currentScrollPosition = window.scrollY;
+
+    if (Math.abs(currentScrollPosition - lastScrollPosition) > delta) {
+      if (currentScrollPosition > lastScrollPosition) {
+        // Scrolling down
+        header.classList.add("hidden");
+      } else {
+        // Scrolling up
+        header.classList.remove("hidden");
+      }
+      lastScrollPosition = currentScrollPosition;
+    }
+
+    ticking = false;
+  }
+
+  window.addEventListener("scroll", () => {
+    if (!isMenuOpen) {
+      if (!ticking) {
+        window.requestAnimationFrame(handleScroll);
+        ticking = true;
+      }
+    }
+
+    if (window.scrollY > 0) {
+      header.classList.add("active");
+    } else {
+      header.classList.remove("active");
+    }
+  });
+
+  const menu_tl = gsap.timeline({
+    paused: true,
+    defaults: {
+      duration: 0.3,
+      ease: "power3.inOut",
+    },
+  });
+
+  const nav_items_tl = gsap.timeline({
+    paused: true,
+    defaults: {
+      ease: "power4.inOut",
+    },
+  });
+
+  menu_tl
+    .to(".menu_line.top", { y: "6.5px" })
+    .to(".menu_line.bottom", { y: "-6.5px" }, "<")
+    .to(".menu_line.top", { rotate: 225, width: "82%", duration: 0.5 })
+    .to(".menu_line.bottom", { rotate: -45, width: "82%" }, "<")
+    .fromTo(
+      ".navigation_wrapper",
+      { y: "-100%" },
+      { y: "0%", duration: 2 },
+      "-=1.3"
+    );
+
+  nav_items_tl.fromTo(
+    ".nav_animate .nav_item",
+    {
+      y: "150%",
+      opacity: 0,
+      scaleY: 2,
+      transformOrigin: "top",
+    },
+    {
+      y: "0%",
+      opacity: 1,
+      scaleY: 1,
+      stagger: 0.07,
+      delay: 0.4,
+      duration: 1.3,
+    }
+  );
+
+  const menu_trigger = document.querySelector(".menu_trigger");
+
+  menu_trigger?.addEventListener("click", () => {
+    if (!isMenuOpen) {
+      menu_tl.play();
+      // Only play nav_items_tl if it's not already playing or active
+      if (!nav_items_tl.isActive()) {
+        nav_items_tl.play(0);
+      } // always play forward
+    } else {
+      menu_tl.reverse();
+      // Don't reverse nav_items_tl (skip it)
+    }
+    isMenuOpen = !isMenuOpen;
+  });
+}
+
 document.querySelectorAll(".swiper").forEach((swiper) => {
   const swiperInstance = new Swiper(swiper, {
     direction: "horizontal",
