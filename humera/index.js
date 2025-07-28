c12193690297a59d92dda4fd1873f3d8c49c0cfd
Vipul Kumar <email.coex@gmail.com>
2025-07-28 05:59:18 +0530

    abc

--- humera/index.js
+++ humera/index.js
@@ -50,67 +50,69 @@ gsap.set("[word-reveal]", {
 const isLoader = false;
 
 function initReveal() {
-  ScrollTrigger.batch("[basic-reveal],[fade-reveal],[para-reveal]", {
-    start: (scrollInstance) => {
-      const eltrigger = scrollInstance.trigger;
-      const eltriggerHeight = eltrigger.clientHeight * 1.4;
-      // if (eltrigger.hasAttribute("basic-reveal")) {
-      //   return `top-=${eltriggerHeight}px bottom`;
-      // }
-      return "top bottom";
-    },
-    end: (scrollInstance) => {
-      const eltrigger = scrollInstance.trigger;
-      const eltriggerHeight = eltrigger.clientHeight * 1.4;
-      // if (eltrigger.hasAttribute("basic-reveal")) {
-      //   return `top-=${eltriggerHeight}px bottom`;
-      // }
-      return "top bottom";
-    },
-    // markers: true,
-    onEnter: (elements, triggers) => {
-      const animateItems = [];
-      let duration = 1;
+  ScrollTrigger.batch(
+    "[basic-reveal],[fade-reveal],[para-reveal],[word-reveal]",
+    {
+      start: (scrollInstance) => {
+        const eltrigger = scrollInstance.trigger;
+        const eltriggerHeight = eltrigger.clientHeight * 1.4;
+        // if (eltrigger.hasAttribute("basic-reveal")) {
+        //   return `top-=${eltriggerHeight}px bottom`;
+        // }
+        return "top bottom";
+      },
+      end: (scrollInstance) => {
+        const eltrigger = scrollInstance.trigger;
+        const eltriggerHeight = eltrigger.clientHeight * 1.4;
+        // if (eltrigger.hasAttribute("basic-reveal")) {
+        //   return `top-=${eltriggerHeight}px bottom`;
+        // }
+        return "top bottom";
+      },
+      // markers: true,
+      onEnter: (elements, triggers) => {
+        const animateItems = [];
 
-      elements.forEach((element) => {
-        if (element.hasAttribute("basic-reveal")) {
-          animateItems.push(element);
-        }
-        if (element.hasAttribute("para-reveal")) {
-          // console.log("para", element);
-          element.querySelectorAll(".para_line").forEach((line) => {
-            animateItems.push(line);
-          });
-        }
-        if (element.hasAttribute("word-reveal")) {
-          // console.log("word", element);
-          element.querySelectorAll(".para_word").forEach((word) => {
-            console.log("word", word);
-            animateItems.push(word);
-          });
-        }
-        if (element.hasAttribute("fade-reveal")) {
-          animateItems.push(element);
-        }
-      });
-      // console.log("animateItems", animateItems);
-
-      gsap.to(animateItems, {
-        y: "0%",
-        opacity: 1,
-        filter: "blur(0px)",
-        scaleY: 1,
-        stagger: 0.04,
-        duration: (index, target) => {
-          if (target.hasAttribute("extra-time")) {
-            return 2.5;
+        elements.forEach((element) => {
+          if (element.hasAttribute("basic-reveal")) {
+            animateItems.push(element);
           }
-          return 1.5;
-        },
-        ease: "power4.inOut",
-      });
-    },
-  });
+          if (element.hasAttribute("para-reveal")) {
+            // console.log("para", element);
+            element.querySelectorAll(".para_line").forEach((line) => {
+              animateItems.push(line);
+            });
+          }
+          if (element.hasAttribute("word-reveal")) {
+            // console.log("word", element);
+            element.querySelectorAll(".para_word").forEach((word) => {
+              console.log("word", word);
+              animateItems.push(word);
+            });
+          }
+          if (element.hasAttribute("fade-reveal")) {
+            animateItems.push(element);
+          }
+        });
+        // console.log("animateItems", animateItems);
+
+        gsap.to(animateItems, {
+          y: "0%",
+          opacity: 1,
+          filter: "blur(0px)",
+          scaleY: 1,
+          stagger: 0.04,
+          duration: (index, target) => {
+            if (target.hasAttribute("extra-time")) {
+              return 2.5;
+            }
+            return 1;
+          },
+          ease: "power4.inOut",
+        });
+      },
+    }
+  );
 }
 
 if (isLoader) {
