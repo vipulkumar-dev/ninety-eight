52d2deaa406a42cf890a7b501b37f002875051d1
Vipul Kumar <email.coex@gmail.com>
2025-09-04 10:36:56 +0530

    abc

--- stardust/index.js
+++ stardust/index.js
@@ -341,13 +341,13 @@ initReveal();
             console.log("play");
           } else {
             // Video is out of view - pause and hide it
-            video.pause();
+            video.reset();
             console.log("pause");
           }
         });
       },
       {
-        threshold: 0.5,
+        threshold: 0,
       }
     );
 
