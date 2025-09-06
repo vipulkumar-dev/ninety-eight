9976c9c1329ed04f14c471466a28d2bcf46f1b92
Vipul Kumar <email.coex@gmail.com>
2025-09-06 10:28:49 +0530

    abc

--- stardust/index.js
+++ stardust/index.js
@@ -22,7 +22,61 @@ gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
   });
 })();
 
+/*
+Returns a FUNCTION that you can feed an element to get its scroll position.
+- targets: selector text, element, or Array of elements
+- config: an object with any of the following optional properties:
+- start: defaults to "top top" but can be anything like "center center", "100px 80%", etc. Same format as "start" and "end" ScrollTrigger values.
+- containerAnimation: the horizontal scrolling tween/timeline. Must have an ease of "none"/"linear".
+- pinnedContainer: if you're pinning a container of the element(s), you must define it so that ScrollTrigger can make the proper accommodations.
+*/
+function getScrollLookup(
+  targets,
+  { start, pinnedContainer, containerAnimation }
+) {
+  let triggers = gsap.utils.toArray(targets).map((el) =>
+      ScrollTrigger.create({
+        trigger: el,
+        start: start || "top top",
+        pinnedContainer: pinnedContainer,
+        refreshPriority: -10,
+        containerAnimation: containerAnimation,
+      })
+    ),
+    st = containerAnimation && containerAnimation.scrollTrigger;
+  return (target) => {
+    let t = gsap.utils.toArray(target)[0],
+      i = triggers.length;
+    while (i-- && triggers[i].trigger !== t) {}
+    if (i < 0) {
+      return console.warn("target not found", target);
+    }
+    return containerAnimation
+      ? st.start +
+          (triggers[i].start / containerAnimation.duration()) *
+            (st.end - st.start)
+      : triggers[i].start;
+  };
+}
+
 (function initNavItemScroll() {
+  // Initialize scroll lookup after ScrollTriggers are created
+  let getPosition;
+
+  // Wait for ScrollTriggers to initialize, then create lookup
+  gsap.delayedCall(0.1, () => {
+    // Get all target elements for the lookup
+    const targetElements = [];
+    document.querySelectorAll(".nav_item").forEach((link) => {
+      const targetId = link.getAttribute("data-href");
+      if (targetId) targetElements.push(targetId);
+    });
+
+    getPosition = getScrollLookup(targetElements, {
+      start: "top top",
+    });
+  });
+
   document.querySelectorAll(".nav_item").forEach((link) => {
     link.addEventListener("click", (e) => {
       e.preventDefault();
@@ -31,10 +85,10 @@ gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
       const targetEl = document.querySelector(`${targetId}`);
       console.log(targetEl);
 
-      if (targetEl) {
+      if (targetEl && getPosition) {
         gsap.to(window, {
           duration: 1,
-          scrollTo: targetEl,
+          scrollTo: getPosition(targetId), // Use calculated position
           ease: "power2.inOut",
         });
       }
