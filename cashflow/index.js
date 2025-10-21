5e696560629bddef556e902b01f3cc5dac90c8e1
Vipul Kumar <email.coex@gmail.com>
2025-10-22 04:13:22 +0530

    Add popup initialization functionality in utils.js
    - Introduced `popup_init` function to manage popup animations and interactions using GSAP.
    - Integrated popup triggers and close triggers for user interaction in cashflow/index.js, initializing the popup for the "waitlist" element.

--- cashflow/index.js
+++ cashflow/index.js
@@ -1,4 +1,4 @@
-import { roll, getDevices, lenisInit } from "../utils.js";
+import { roll, getDevices, lenisInit, popup_init } from "../utils.js";
 import { liveReload } from "../liveReload.js";
 
 const { isDesktop, isMobile } = getDevices();
@@ -394,6 +394,7 @@ try {
   console.log("err", err);
 }
 
+popup_init("waitlist");
 // Handle window resize to start/stop auto-swipe
 window.addEventListener("resize", () => {
   if (isMobile) {
