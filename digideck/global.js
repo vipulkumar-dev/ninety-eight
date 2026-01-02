1bd1e35852fbc072ed87a2556578fec329436232
Vipul Kumar <email.coex@gmail.com>
2026-01-02 14:28:51 +0530

    Add scroll-triggered header visibility and button hover effects
    - Implemented a scroll event listener to toggle the header's visibility based on scroll direction, enhancing user experience on desktop and mobile.
    - Added CSS styles for button hover effects, including a background gradient and animation for a more dynamic interaction.

--- digideck/global.js
+++ digideck/global.js
@@ -3,6 +3,46 @@ import { liveReload } from "../liveReload.js";
 
 const { isDesktop, isMobile } = getDevices();
 
+const header = document.getElementById("header");
+if (header) {
+  let isMenuOpen = false;
+  let lastScrollPosition = 0;
+  let delta = isDesktop ? 70 : 100; // Minimum scroll distance before toggling header
+  let ticking = false;
+
+  function handleScroll() {
+    const currentScrollPosition = window.scrollY;
+
+    if (Math.abs(currentScrollPosition - lastScrollPosition) > delta) {
+      if (currentScrollPosition > lastScrollPosition) {
+        // Scrolling down
+        header.classList.add("hidden");
+      } else {
+        // Scrolling up
+        header.classList.remove("hidden");
+      }
+      lastScrollPosition = currentScrollPosition;
+    }
+
+    ticking = false;
+  }
+
+  window.addEventListener("scroll", () => {
+    if (!isMenuOpen) {
+      if (!ticking) {
+        window.requestAnimationFrame(handleScroll);
+        ticking = true;
+      }
+    }
+
+    if (window.scrollY > 0) {
+      header.classList.add("active");
+    } else {
+      header.classList.remove("active");
+    }
+  });
+}
+
 (function faq_init() {
   const faq_items = document.querySelectorAll(".faq_item");
   let activeIndex = null;
@@ -92,7 +132,7 @@ const { isDesktop, isMobile } = getDevices();
 
     // Parse data-x attribute (default: -50, 30)
     const dataX = mouse_follow_item.getAttribute("strength-x");
-    const xRange = dataX ? parseFloat(dataX.trim()) : 20;
+    const xRange = dataX ? parseFloat(dataX.trim()) : 10;
 
     // Parse data-y attribute (default: -50, 30)
     const dataY = mouse_follow_item.getAttribute("strength-y");
